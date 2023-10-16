9828c7a644db5c06d9a02fe80c387a7e99fbab3d
Mohamed Yehia <eng.essa85@gmail.com>
2023-10-16 13:18:32 +0300

    adding the project files

---
+++ javascripts/index.js
@@ -0,0 +1,9 @@
+
+
+const button = document.querySelector(".button");
+
+const HandelGoingToAboutPage = () => {
+  window.location.href = "about.html";
+};
+
+button.addEventListener("click", HandelGoingToAboutPage);
